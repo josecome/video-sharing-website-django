5be853cdaa9da563edc5da51e61c8995258431d6
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-04 19:03:30 +0200

    Minor changes made

--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from models import (
+    Category,
+    Video
+)
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = {}
+    videos = Video.objects.all()[:8]
+    context['videos'] = videos
+    return render(request, 'home.html', context)
